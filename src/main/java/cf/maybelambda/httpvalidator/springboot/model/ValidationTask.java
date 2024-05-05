6b7dd3e9c80254f7c0a00e7a33f3b7bdeee18b5c
Asta86 <psljp@protonmail.com>
2024-05-05 17:09:25 -0300

    Declare args that cannot be null

--- src/main/java/cf/maybelambda/httpvalidator/springboot/model/ValidationTask.java
+++ src/main/java/cf/maybelambda/httpvalidator/springboot/model/ValidationTask.java
@@ -1,10 +1,20 @@
 package cf.maybelambda.httpvalidator.springboot.model;
 
+import org.springframework.lang.NonNull;
+
 import java.util.List;
 
-public record ValidationTask(int reqMethod, String reqURL, List<String> reqHeaders, int validStatusCode, String validBody) {
+import static java.util.Objects.requireNonNull;
 
-    public boolean isValid(int statusCode, String body) {
+
+public record ValidationTask(int reqMethod, String reqURL, List<String> reqHeaders, int validStatusCode, String validBody) {
+    public ValidationTask {
+        requireNonNull(reqURL);
+        requireNonNull(reqHeaders);
+        requireNonNull(validBody);
+    }
+
+    public boolean isValid(int statusCode, @NonNull String body) {
         return this.validStatusCode == statusCode && body.contains(this.validBody);
     }
 }
